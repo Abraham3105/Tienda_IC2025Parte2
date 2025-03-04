ad9808f51e7ba88bddeae702532ae4a065730bc6
Abraham3105 <acascantebadilla31@gmail.com>
2025-03-03 20:43:56 -0600

    Semana 08 commit del Proyecto Tienda

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -2,15 +2,16 @@ package com.tienda.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
 @Entity
-@Table(name="categoria")
-public class Categoria implements Serializable{
-    
+@Table(name = "categoria")
+public class Categoria implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_categoria")
@@ -24,13 +25,18 @@ public class Categoria implements Serializable{
 
     @Column(name = "activo")
     private boolean activo;
-    
-    public Categoria(){
-        
+
+    @OneToMany
+    @JoinColumn(name = "id_categoria")
+    List<Producto> productos;
+
+    public Categoria() {
+
     }
-    public Categoria(String descripcion, boolean activo){
+
+    public Categoria(String descripcion, boolean activo) {
         this.descripcion = descripcion;
         this.activo = activo;
     }
-    
+
 }
